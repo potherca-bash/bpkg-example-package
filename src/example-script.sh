234889f615df0f4ee8dfa39f25cbf0c45e1c722a
Ben Peachey <potherca@gmail.com>
2017-07-07 20:25:32 +0000

    Changes main script to be more robust.

--- src/example-script.sh
+++ src/example-script.sh
@@ -1,18 +1,19 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 source 'comment.license.inc'
 source 'comment.code-style.inc'
 
+source 'declare.bash-options.inc'
+
 source 'declare.exit-codes.inc'
 
-VERSION=0.0.1
-
+readonly VERSION=0.0.1
 source 'function.usage.sh'
 source 'function.example_script.sh'
 
 if [[ ${BASH_SOURCE[0]} != $0 ]]; then
-  export -f example_script
+    export -f example_script
 else
-  example-script "${@}"
-  exit $?
+    example-script "${@:-}"
+    exit $?
 fi
